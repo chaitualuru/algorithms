0612b280d763ef1280745d063c4acfbf7c927af3
kcaluru <krishna_aluru@brown.edu>
2015-06-02 14:37:40 +0530

    rewrote ordered min max

--- interview_cake/ordered_min_max.py
+++ interview_cake/ordered_min_max.py
@@ -1,7 +1,21 @@
-def get_best_profit(prices):
+# keep track of max_profit by using min_price
+
+def get_max_profit(prices):
 	min_price = prices[0]
-	max_profit = 0
-	for p in prices:
-		min_price = min(p, min_price)
-		max_profit = max(max_profit, p - min_price)
+	cur_price = prices[1]
+	max_profit = cur_price - min_price
+	
+	for index, cur_price in enumerate(prices):
+		
+		if index < 1:
+			continue
+		
+		min_price = min(min_price, prices[index - 1])
+		
+		max_profit = max(max_profit, cur_price - min_price)
+		
 	return max_profit
+	
+
+print get_max_profit([5, -12, 17, 19])
+		
